806dba658cc2e4b5ea3de91cdad8ccd27294818e
aaronphy <aaronphy@163.com>
2023-07-24 22:44:27 +0800

    update code for init push

---
+++ src/vite-env.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="vite/client" />
+
+
+declare module '@editorjs/*'
+
+declare module 'editorjs-undo'
+
+declare module 'editorjs-drag-drop'
+
+declare module '@sotaproject/strikethrough'
+
+declare module 'editorjs-text-color-plugin'
+
+declare module 'editorjs-text-alignment-blocktune'
+
+declare module '@calumk/editorjs-codeflask'
